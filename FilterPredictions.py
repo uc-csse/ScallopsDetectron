42af67510a35b06d90037c9d18e334240f1880f9
Tim <timrensen@yahoo.com>
2024-11-18 08:41:16 +1300

    tuning filter

--- FilterPredictions.py
+++ FilterPredictions.py
@@ -99,7 +99,7 @@ def process_dir(base_dir, dirname):
         scores = np.sort([polygon_scores[p_idx] for p_idx in c_idxs])
         combined_score = np.sum(scores[-CLUSTER_TOP_N:])
         scores_debug.append(combined_score)
-        if combined_score > CLUSTER_TOPN_SCORE_THRESH:  # scores[-1] > 0.95:  #
+        if scores[-1] > 0.90:  # combined_score > CLUSTER_TOPN_SCORE_THRESH:  #
             if len(cluster_polygons) > 1:
                 combined_polygon = spf.cluster_avg_polygon(cluster_polygons, scores)
             else:
